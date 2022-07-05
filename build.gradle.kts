14fda71f82685ff1c5d767584f32baf4ad2294c2
Adam <897017+aSemy@users.noreply.github.com>
2022-07-05 14:16:00 +0200

    tidy up old comments (code was moved to buildSrc)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,34 +1,12 @@
 plugins {
-//    kotlin("jvm")
 //    id("org.jlleitschuh.gradle.ktlint") version "10.1.0" // TODO re-enable ktlint
     id("com.palantir.git-version") version "0.15.0"
 }
-//
+
 val gitVersion: groovy.lang.Closure<String> by extra
-//
+
 group = "codes.laurence.warden"
 version = gitVersion().replace(".dirty", "")
-//
-//subprojects {
-// TODO move ktlint settings to buildSrc plugin
-//    apply(plugin = "org.jlleitschuh.gradle.ktlint")
-//
-//    configure<org.jlleitschuh.gradle.ktlint.KtlintExtension> {
-//        disabledRules.set(
-//            setOf("no-wildcard-imports", "filename")
-//        )
-//    }
-//}
-//
-//buildscript {
-//    repositories {
-//        mavenCentral()
-////        jcenter()
-//    }
-//    dependencies {
-//        classpath("org.jfrog.buildinfo:build-info-extractor-gradle:4.21.0")
-//    }
-//}
 
 tasks.wrapper {
     gradleVersion = "7.4.2"
