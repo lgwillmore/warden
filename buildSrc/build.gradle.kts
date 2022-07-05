f2feeddc84ba42bb52ad1f66edb2d2daf039c836
Adam <897017+aSemy@users.noreply.github.com>
2022-07-05 18:51:24 +0200

    tidy up and document buildSrc/build.gradle.kts

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -1,46 +1,50 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-  `kotlin-dsl`
-  kotlin("jvm") version "1.6.21"
+    `kotlin-dsl`
+    kotlin("jvm") version "1.6.21"
+    // Gradle uses an embedded Kotlin with version 1.4
+    // https://docs.gradle.org/current/userguide/compatibility.html#kotlin
+    // but it's safe to use 1.6.21, as long as the language level is set to 1.4
+    // (the kotlin-dsl plugin does this).
 }
 
+// set the versions of Gradle plugins that the subprojects will use here
+val kotlinPluginVersion: String = "1.6.21"
+val artifactoryPluginVersion: String = "4.28.4"
+val orchidPluginVersion: String = "0.21.1"
+val ktlintPluginVersion: String = "10.3.0"
+
+
 dependencies {
-    implementation(enforcedPlatform("org.jetbrains.kotlin:kotlin-bom:1.6.21"))
+    implementation(platform("org.jetbrains.kotlin:kotlin-bom:$kotlinPluginVersion"))
     implementation("org.jetbrains.kotlin:kotlin-serialization")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-compiler-embeddable")
-    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.21")
+    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlinPluginVersion")
 
-    // TODO Verify if build-info-extractor-gradle is still required
-    implementation("org.jfrog.buildinfo:build-info-extractor-gradle:4.21.0")
+    implementation("org.jfrog.buildinfo:build-info-extractor-gradle:$artifactoryPluginVersion")
 
-    implementation("gradle.plugin.com.eden:orchidPlugin:0.21.1")
+    implementation("gradle.plugin.com.eden:orchidPlugin:$orchidPluginVersion")
 
-    implementation("org.jlleitschuh.gradle:ktlint-gradle:10.3.0")
+    implementation("org.jlleitschuh.gradle:ktlint-gradle:$ktlintPluginVersion")
 }
 
 val gradleJvmTarget = "11"
 
 tasks.withType<KotlinCompile>().configureEach {
 
-  kotlinOptions {
-    jvmTarget = gradleJvmTarget
-  }
-
-  kotlinOptions.freeCompilerArgs += listOf(
-    "-opt-in=kotlin.RequiresOptIn",
-    "-opt-in=kotlin.ExperimentalStdlibApi",
-    "-opt-in=kotlin.time.ExperimentalTime",
-  )
+    kotlinOptions {
+        jvmTarget = gradleJvmTarget
+    }
 }
 
 kotlin {
-  jvmToolchain {
-    (this as JavaToolchainSpec).languageVersion.set(JavaLanguageVersion.of(gradleJvmTarget))
-  }
+    jvmToolchain {
+        (this as JavaToolchainSpec).languageVersion.set(JavaLanguageVersion.of(gradleJvmTarget))
+    }
 
-  kotlinDslPluginOptions {
-    jvmTarget.set(gradleJvmTarget)
-  }
+    kotlinDslPluginOptions {
+        jvmTarget.set(gradleJvmTarget)
+    }
 }
